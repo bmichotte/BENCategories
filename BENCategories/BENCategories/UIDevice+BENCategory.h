26cb1afeeb4ae99913403d4c9c6c4040b4fee389
Benjamin Michotte <bmichotte@gmail.com>
2013-10-24 18:31:11 +0200

    Ajout catégories UIDevice

---
+++ BENCategories/BENCategories/UIDevice+BENCategory.h
@@ -0,0 +1,71 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Benjamin Michotte <bmichotte@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (BENCategory)
+
+/**
+ Check if the device is an iPad
+
+ @return YES if it is an iPad
+ */
++ (BOOL)isPad;
+
+/**
+ Check if the device is an iPhone or iPod
+
+ @return YES if it is an iPhone or iPod
+ */
++ (BOOL)isPhone;
+
+/**
+ Check if the device is an iPhone 5
+
+ @return YES if it is an iPhone 5
+ */
++ (BOOL)isPhone5;
+
+/**
+ Check if the device is a Retina model
+
+ @return YES if it is a Retina model
+ */
++ (BOOL)isRetina;
+
+/**
+ Check if the device is in portrait mode
+
+ @return YES if it is in portrait mode
+ */
++ (BOOL)isPortrait;
+
+/**
+ Check if the device is in landscape mode
+
+ @return YES if it is in landscape mode
+ */
++ (BOOL)isLandscape;
+
+@end
---
+++ BENCategories/BENCategories/UIDevice+BENCategory.m
@@ -0,0 +1,77 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Benjamin Michotte <bmichotte@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#import "UIDevice+BENCategory.h"
+
+@implementation UIDevice (BENCategory)
+
++ (BOOL)isPad
+{
+    return UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad;
+}
+
++ (BOOL)isPhone
+{
+    return UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone;
+}
+
++ (BOOL)isPhone5
+{
+    if (![[self class] isPhone])
+    {
+        return NO;
+    }
+
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)])
+    {
+        CGSize result = [[UIScreen mainScreen] bounds].size;
+        CGFloat scale = [UIScreen mainScreen].scale;
+        result = CGSizeMake(result.width * scale, result.height * scale);
+
+        if (result.height == 1136.0)
+        {
+            return YES;
+        }
+    }
+    return NO;
+}
+
++ (BOOL)isRetina
+{
+    return [[UIScreen mainScreen] scale] > 1.0;
+}
+
++ (BOOL)isPortrait
+{
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    return UIInterfaceOrientationIsPortrait(orientation);
+}
+
++ (BOOL)isLandscape
+{
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    return UIInterfaceOrientationIsLandscape(orientation);
+}
+
+@end
